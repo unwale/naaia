e659886795532e82f883dd6778ec0c5c171d4d14
Nikita Dubrovin <ndubrovin22@gmail.com>
2024-12-06 15:36:03 +0300

    fix tokenizer max length argument name

--- scripts/training/topic_classification/train_all.sh
+++ scripts/training/topic_classification/train_all.sh
@@ -3,12 +3,12 @@
 python -m scripts.training.topic_classification.sgd
 
 python -m scripts.training.topic_classification.bert_lin_text --model cointegrated/rubert-tiny2
-python -m scripts.training.topic_classification.bert_lin_text --model DeepPavlov/rubert-base-cased --model_max_length 512
+python -m scripts.training.topic_classification.bert_lin_text --model DeepPavlov/rubert-base-cased --tokenizer_max_length 512
 python -m scripts.training.topic_classification.bert_lin_text --model ai-forever/ruBert-large
 
 python -m scripts.training.topic_classification.bert_mlp_text --model cointegrated/rubert-tiny2
-python -m scripts.training.topic_classification.bert_mlp_text --model DeepPavlov/rubert-base-cased --model_max_length 512
+python -m scripts.training.topic_classification.bert_mlp_text --model DeepPavlov/rubert-base-cased --tokenizer_max_length 512
 python -m scripts.training.topic_classification.bert_mlp_text --model ai-forever/ruBert-large
 
 python -m scripts.training.topic_classification.bert_multifeature.py --model cointegrated/rubert-tiny2
-python -m scripts.training.topic_classification.bert_multifeature.py --model DeepPavlov/rubert-base-cased --model_max_length 512
+python -m scripts.training.topic_classification.bert_multifeature.py --model DeepPavlov/rubert-base-cased --tokenizer_max_length 512
